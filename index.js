177498bf13bdf5b50132958c66de4a24fd340de1
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-04 16:10:21 +0530

    Node

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const http = require("http")
-const fs = require('fs')
+// const http = require("http")
+// const fs = require('fs')
 
-const myServer = http.createServer((req, res)=>{
-    const log = `${Date.now()} : New Req Recived\n`
-  fs.appendFile('log.txt',log,(err,data)=>{
-    res.end('Hello From Server Again')
-  } )
+// const myServer = http.createServer((req, res)=>{
+//     const log = `${Date.now()} : New Req Recived\n`
+//   fs.appendFile('log.txt',log,(err,data)=>{
+//     res.end('Hello From Server Again')
+//   } )
  
+// })
+
+// myServer.listen(8000,()=> console.log("Server Started") )
+
+
+const http = require('http')
+
+
+
+const Myserver =  http.createServer((req, res)=>{
+  const log = 
+  res.end("Server Started")
+
 })
 
-myServer.listen(8000,()=> console.log("Server Started") )
+Myserver.listen(8000, ()=> console.log("On Port 8000") )
