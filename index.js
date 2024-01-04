cde16cb459789a165df7ef2d91962ca307e34fa6
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-04 16:58:19 +0530

    Changes

--- index.js
+++ index.js
@@ -13,12 +13,32 @@
 
 
 const http = require('http')
+const fs = require('fs')
+const url = require("url")
 
 
 
 const Myserver =  http.createServer((req, res)=>{
-  const log = 
-  res.end("Server Started")
+  if(req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()}: ${req.url} ${req.method} New Req Recived\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
+  fs.appendFile("log.txt", log,(err,data)=>{
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("Homepage")
+        break;
+    
+      case "/about":
+        res.end("I am Atharv Dalal")
+        break;
+    
+      default:
+        res.end("404 Not Found")
+    }
+  })
+
+
 
 })
 
