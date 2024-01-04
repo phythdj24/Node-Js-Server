3d9e9abb52eddc2173004bf4dee1ec6ed5321ae3
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-04 17:17:07 +0530

    chnages

--- index.js
+++ index.js
@@ -13,33 +13,52 @@
 
 
 const http = require('http')
-const fs = require('fs')
-const url = require("url")
 
+const express = require('express')
 
-
-const Myserver =  http.createServer((req, res)=>{
-  if(req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}: ${req.url} ${req.method} New Req Recived\n`;
-  const myUrl = url.parse(req.url, true);
-  console.log(myUrl);
-  fs.appendFile("log.txt", log,(err,data)=>{
-    switch (myUrl.pathname) {
-      case "/":
-        res.end("Homepage")
-        break;
-    
-      case "/about":
-        res.end("I am Atharv Dalal")
-        break;
-    
-      default:
-        res.end("404 Not Found")
-    }
-  })
-
-
-
+const app = express()
+app.get("/", (req,res)=>{
+  return res.send("Hello From Home Express")
+})
+app.get("/about", (req,res)=>{
+  return res.send("Hello From About Express" + "Hey" + req.query.name)
 })
 
-Myserver.listen(8000, ()=> console.log("On Port 8000") )
+const myServer = http.createServer(app)
+
+
+myServer.listen(8000, ()=> console.log("On Port 8000") )
+
+
+
+
+
+
+
+
+
+
+
+// const Myserver =  http.createServer((req, res)=>{
+//   if(req.url === "/favicon.ico") return res.end();
+//   const log = `${Date.now()}: ${req.url} ${req.method} New Req Recived\n`;
+//   const myUrl = url.parse(req.url, true);
+//   console.log(myUrl);
+//   fs.appendFile("log.txt", log,(err,data)=>{
+//     switch (myUrl.pathname) {
+//       case "/":
+//         res.end("Homepage")
+//         break;
+    
+//       case "/about":
+//         res.end("I am Atharv Dalal")
+//         break;
+    
+//       default:
+//         res.end("404 Not Found")
+//     }
+//   })
+
+
+
+// })
